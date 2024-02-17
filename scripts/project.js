d3f6d61d19dd1087e52a2584da7c945b89519b45
ebark24 <elibarker@outlook.com>
2024-02-16 22:52:57 -0600

    Start of final project

---
+++ scripts/project.js
@@ -0,0 +1,58 @@
+const climbingEmelemt = document.getElementById("climbing locations");
+let climbingList = [];
+
+const displayClimbs = (climbingList) => {
+    climbingList.forEach(climb => {
+        let article = document.createElement("article");
+        let climbName = document.createElement("h3");
+        climbName.textContent = climb.climbName;
+        let imageUrl = document.createElement("img");
+        imageUrl.setAttribute('src', climb.imageUrl);
+        imageUrl.setAttribute('alt', temple.location);
+        article.appendChild(climbName);
+        article.appendChild(imageUrl);
+        climbingEmelemt.appendChild(article);
+    });
+};
+
+/* async getTemples Function using fetch()*/
+const getClimbs = async () => {
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    const data = await response.json();
+    climbingList = data;
+    displayClimbs();
+};
+
+/* reset Function */
+const reset = () => {
+    climbingEmelemt.innerHTML = '';
+};
+
+/* filter the types of climbs */
+
+const filterClimbs = (climbs) => {
+    reset()
+    let filter = document.querySelector("#filtered").value
+    switch (filter) {
+        case "Sport Climbing":
+            let sportClimbs = climbs.filter(climb => climb.type.toLowerCase().includes("Sport Climb"));
+            displayTemples(sportClimbs); 
+            break;
+        case "Bouldering":
+            let BoulderingClimbs = climbs.filter(climb => climb.type.toLowerCase().includes("Bouldering"));
+            displayTemples(BoulderingClimbs); 
+            break;
+        case "Trad Climbing":
+            let TradClimbs = climbs.filter(climb => climb.type.toLowerCase().includes("Trad Climb"));
+            displayTemples(TradClimbs); 
+            break;
+        case "all climbs":
+            displayTemples(climbingList)
+            break;
+    }
+}
+
+getClimbs();
+
+/* Event Listener */
+document.querySelector("#filtered").addEventListener("change", () => {filterClimbs(climbingList)})
